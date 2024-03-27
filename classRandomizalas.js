2dcbadc54a5673cf88863deff5be5b1b97e14af4
lajkogege <lajkogege@gmail.com>
2024-03-27 12:45:38 +0100

    radnom

--- classRandomizalas.js
+++ classRandomizalas.js
@@ -1,9 +1,9 @@
-
-function randomizalas(classNevek){
-    for (let index = 0; index < classNevek.lenght; index++) {
-        let random=Math.floor(Math.random()*(classNevek.lenght-index))+index;
-        let csere= classNevek[index];
-        classNevek[i]=classNevek[random];
-        classNevek[random]=csere;
-    }
+function randomizalas(classNevek) {
+  for (let index = 0; index < 9; index++) {
+    let random =
+      Math.floor(Math.random() * (classNevek.lenght - index)) + index;
+    let csere = classNevek[index];
+    classNevek[i] = classNevek[random];
+    classNevek[random] = csere;
+  }
 }
