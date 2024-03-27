7c2e15993771f71965f59df63aa774d132e26e16
KAkos27 <akoskosztolanyi@gmail.com>
2024-03-27 13:28:40 +0100

    kis ez kis az

--- lampaKattint.js
+++ lampaKattint.js
@@ -1,9 +1,11 @@
-function lampaKattint(event) {
-  if (event.target === "felkapcsolt") {
-    event.target.removeClass("felkapcsolt");
-    event.target.addClass("lekapcsolt");
+function lampaKattint(event, i) {
+  if (event.target.className === "felkapcsolt") {
+    $(event.target).removeClass("felkapcsolt");
+    $(event.target).addClass("lekapcsolt");
   } else {
-    event.target.removeClass("lekapcsolt");
-    event.target.addClass("felkapcsolt");
+    $(event.target).removeClass("lekapcsolt");
+    $(event.target).addClass("felkapcsolt");
   }
 }
+
+export default lampaKattint;
