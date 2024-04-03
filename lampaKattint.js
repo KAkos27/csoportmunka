dc8b0977fd813990c6322c781c61d482ece6f1da
leventepatrik <levente.paroczi@gmail.com>
2024-04-03 13:12:06 +0200

    sd

--- lampaKattint.js
+++ lampaKattint.js
@@ -8,18 +8,19 @@ const atszinez = (lampa, lampaClassNev) => {
     lampa.addClass("felkapcsolt");
   }
 };
-
 function lampaKattint(event, i, gombok) {
   const lampa = $(event.target);
   const lampaClassNev = event.target.className;
   atszinez(lampa, lampaClassNev);
-
+/*
   if (i === 4) {
     atszinez(gombok.eq(i - 3), gombok.eq(i - 3).className);
     atszinez(gombok.eq(i + 3), gombok.eq(i + 3).className);
     atszinez(gombok.eq(i - 1), gombok.eq(i - 1).className);
     atszinez(gombok.eq(i + 1), gombok.eq(i + 1).className);
   }
+  */
 }
 
 export default lampaKattint;
+
