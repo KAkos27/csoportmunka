6ae5e0f17ffeb035ca0a760b65608d01722bd15a
lajkogege <lajkogege@gmail.com>
2024-04-03 13:41:22 +0200

    gombok

--- lampaKattint.js
+++ lampaKattint.js
@@ -1,5 +1,6 @@
 const atszinez = (lampa, lampaClassNev) => {
-
+console.log(lampaClassNev);
+console.log(lampa)
   if (lampaClassNev === "felkapcsolt") {
     lampa.removeClass("felkapcsolt");
     lampa.addClass("lekapcsolt");
@@ -23,15 +24,27 @@ function lampaKattint(event, i, gombok) {
   }
 */
 
-
+/*
   if (i === 4) {
     atszinez(gombok.eq(i - 3), gombok.eq(i - 3).className);
     atszinez(gombok.eq(i + 3), gombok.eq(i + 3).className);
     atszinez(gombok.eq(i - 1), gombok.eq(i - 1).className);
     atszinez(gombok.eq(i + 1), gombok.eq(i + 1).className);
-  }
-}
+  }*/
 
+  
+    if(i%3!==0){
+      atszinez(gombok.eq(i - 1), gombok.eq(i - 1).attr("class"));//balra
+    }
+
+    if(i)
+
+    atszinez(gombok.eq(i - 3), gombok.eq(i - 3).attr("class"));//felette
+    atszinez(gombok.eq(i + 3), gombok.eq(i + 3).attr("class"));//alatta
+ 
+    atszinez(gombok.eq(i + 1), gombok.eq(i + 1).attr("class")); //jobbra
+
+};
 
 
 export default lampaKattint;
